d1355ed161845db60e9b2596e45fb94b49bd3b4a
SergioRosello <sergio.rosello@hotmail.com>
2024-02-26 18:20:13 +0100

    Finished chapter 19

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"os"
 	"runtime"
 	"strings"
@@ -17,13 +18,14 @@ import (
 	"github.com/SergioRosello/greenlight/internal/data/models"
 	"github.com/SergioRosello/greenlight/internal/jsonlog"
 	"github.com/SergioRosello/greenlight/internal/mailer"
+	"github.com/SergioRosello/greenlight/internal/vcs"
 	_ "github.com/lib/pq"
 )
 
 // Declare a string containing the application version number. Later in the book we'll
 // generate this automatically at build time, but for now we'll just store the version
 // number as a hard-coded global constant.
-const version = "1.0.0"
+var version = vcs.Version()
 
 // Define a config struct to hold all the configuration settings for our application.
 // For now, the only configuration settings will be the network port that we want the
@@ -112,7 +114,15 @@ func main() {
 		return nil
 	})
 
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	// Display version and exit
+	if *displayVersion {
+		fmt.Printf("version: %s\n", version)
+		os.Exit(0)
+	}
 
 	// Initialize a new logger which writes messages to the standard out stream,
 	// prefixed with the current date and time.
